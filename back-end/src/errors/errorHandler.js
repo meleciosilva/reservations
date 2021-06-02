bd7cc0d6a9b6be5009515c394510556aa87c52ee
meleciosilva <mpsilva94@gmail.com>
2021-06-02 12:57:13 -0400

    add Validation Error

--- back-end/src/errors/errorHandler.js
+++ back-end/src/errors/errorHandler.js
@@ -1,7 +1,17 @@
 /**
  * Express API error handler.
  */
+
+const { ValidationError } = require('express-validation');
+
 function errorHandler(error, request, response, next) {
+  
+  // returns error from createReservationValidation 
+  if (error instanceof ValidationError) {
+    return response.status(error.statusCode).json(error)
+  }
+  
+  // handles other errors
   const { status = 500, message = "Something went wrong!" } = error;
   response.status(status).json({ error: message });
 }
