ebdfa33ec74dcb0103ef3cb9fdd05ad0f5cb5896
meleciosilva <mpsilva94@gmail.com>
2021-06-02 16:31:57 -0400

    add date to search query params

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -4,6 +4,7 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
+import useQuery from "./../utils/useQuery";
 
 /**
  * Defines all the routes for the application.
@@ -13,6 +14,11 @@ import { today } from "../utils/date-time";
  * @returns {JSX.Element}
  */
 function Routes() {
+  
+  // sets date to date found in query or today's date by default
+  const query = useQuery();
+  const date = query.get("date") || today();
+  
   return (
     <Switch>
       <Route exact={true} path="/">
@@ -22,7 +28,7 @@ function Routes() {
         <Redirect to={"/dashboard"} />
       </Route>
       <Route path="/dashboard">
-        <Dashboard date={today()} />
+        <Dashboard date={date} />
       </Route>
       <Route>
         <NotFound />
