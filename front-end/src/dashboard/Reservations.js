dd6af624fb620601d165bf32284f534f9fef3c36
meleciosilva <mpsilva94@gmail.com>
2021-06-03 10:42:11 -0400

    add card component for reservations

--- front-end/src/dashboard/Reservations.js
+++ front-end/src/dashboard/Reservations.js
@@ -3,7 +3,20 @@ function Reservations({ reservations }) {
   if (!reservations) return null;
   return (
     <div>
-      { reservations.map(item => item.first_name) }
+      { reservations.map(reservation => (
+        <div className="card" key={ reservation.reservation_id }>
+          <div className="card-header d-flex justify-content-between">
+            <h5 className="font-weight-bold">{ reservation.first_name } { reservation.last_name }</h5>
+            <h5 className="font-weight-bold">Party: { reservation.people }</h5>
+          </div>
+          <div className="card-body">
+            <h5 className="card-title">Time</h5>
+            <p className="card-text">{ reservation.reservation_time }</p>
+            <h5 className="card-title">Contact Number</h5>
+            <p className="card-text">{ reservation.mobile_number }</p>
+          </div>
+        </div>
+      )) }
     </div>
   )
 }
