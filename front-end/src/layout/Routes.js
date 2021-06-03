e6c80b63f91ecd3c4636017a7f0c955353be055e
meleciosilva <mpsilva94@gmail.com>
2021-06-03 14:38:51 -0400

    add /reservations Route

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
+import Reservations from "../reservations/Reservations";
 import NotFound from "./NotFound";
 
 /**
@@ -18,11 +19,11 @@ function Routes() {
       <Route exact={true} path="/">
         <Redirect to={"/dashboard"} />
       </Route>
-      <Route exact={true} path="/reservations">
-        <Redirect to={"/dashboard"} />
-      </Route>
       <Route path="/dashboard">
         <Dashboard />
+      </Route>
+      <Route path="/reservations">
+        <Reservations />
       </Route>
       <Route>
         <NotFound />
