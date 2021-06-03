9efbb523cb00a0d73f3dad4ef62decab3ff4ec94
meleciosilva <mpsilva94@gmail.com>
2021-06-03 10:41:53 -0400

    add ButtonGroup component and date state + handler

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { listReservations } from "../utils/api";
+import { today } from "../utils/date-time";
+import useQuery from "./../utils/useQuery";
+
 import ErrorAlert from "../layout/ErrorAlert";
+import ButtonGroup from "./ButtonGroup";
 import Reservations from "./Reservations";
 
 /**
@@ -9,9 +13,14 @@ import Reservations from "./Reservations";
  *  the date for which the user wants to view reservations.
  * @returns {JSX.Element}
  */
-function Dashboard({ date }) {
+function Dashboard() {
+  const query = useQuery();
+
   const [reservations, setReservations] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
+  // sets date to date found in query or today's date by default
+  const [date, setDate] = useState(query.get("date") || today());
+  
 
   useEffect(loadDashboard, [date]);
 
@@ -24,14 +33,19 @@ function Dashboard({ date }) {
     return () => abortController.abort();
   }
 
+  function handleDate(newDate) {
+    setDate(newDate);
+  }
+
   return (
     <main>
       <h1>Dashboard</h1>
       <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for date</h4>
+        <h4 className="mb-0">Reservations for { date }</h4>
       </div>
-      <Reservations reservations={reservations} />
+      <ButtonGroup date={date} handleDate={handleDate}/>
       <ErrorAlert error={reservationsError} />
+      <Reservations reservations={reservations} />
     </main>
   );
 }
