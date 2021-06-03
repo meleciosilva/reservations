e9bab009a253608793a8334a69d66cb44f252e6f
meleciosilva <mpsilva94@gmail.com>
2021-06-03 14:36:58 -0400

    change class to className

--- front-end/src/dashboard/ButtonGroup.js
+++ front-end/src/dashboard/ButtonGroup.js
@@ -2,7 +2,7 @@ import { previous, today, next } from "../utils/date-time";
 
 function ButtonGroup({ date, handleDate }) {
   return (
-    <div class="btn-group d-flex justify-content-around pb-2" role="group" aria-label="Basic example">
+    <div className="btn-group d-flex justify-content-around pb-2" role="group" aria-label="Basic example">
       <button type="button" className="btn btn-dark" onClick={ () => handleDate(previous(date)) }>Previous</button>
       <button type="button" className="btn btn-success" onClick={ () => handleDate(today(date)) }>Today</button>
       <button type="button" className="btn btn-dark" onClick={ () => handleDate(next(date)) }>Next</button>
