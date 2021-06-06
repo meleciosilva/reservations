a4cad770824f5a91b7fdcd6f705ec8779ce8f22e
meleciosilva <mpsilva94@gmail.com>
2021-06-06 18:22:38 -0400

    add Seat Link button

--- front-end/src/dashboard/Reservations.js
+++ front-end/src/dashboard/Reservations.js
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Reservations({ reservations }) {
   
   if (!reservations) return null;
   return (
     <div>
+      <h2>Reservations</h2>
       { reservations.map(reservation => (
         <div className="card mb-2" key={ reservation.reservation_id }>
           <div className="card-header d-flex justify-content-between">
@@ -14,6 +17,9 @@ function Reservations({ reservations }) {
             <p className="card-text">{ reservation.reservation_time }</p>
             <h5 className="card-title">Contact Number</h5>
             <p className="card-text">{ reservation.mobile_number }</p>
+          </div>
+          <div className="col-md-12 mb-2">
+            <Link className="btn btn-success" to={`/reservations/${reservation.reservation_id}/seat`}>Seat</Link>
           </div>
         </div>
       )) }
