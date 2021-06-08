277e5a7594bbc9776bd6f86ce7e4ebb225130134
meleciosilva <mpsilva94@gmail.com>
2021-06-07 22:22:15 -0400

    add props to pass to routes

--- front-end/src/reservations/Reservations.js
+++ front-end/src/reservations/Reservations.js
@@ -1,19 +1,20 @@
 import { Redirect, Route, Switch } from "react-router-dom";
-import Seat from "./Seat";
 
+import Seat from "./Seat";
 import NewReservation from "./NewReservation";
 
-function Reservations() {
+function Reservations({ reservations, tables, handleUpdateTable }) {
+
   return (
     <Switch>
       <Route exact={true} path="/reservations">
-        <Redirect to={"/dashboard"} />
+        <Redirect to="/dashboard" />
       </Route>
       <Route path="/reservations/:reservation_id/seat">
-        <Seat />
+        <Seat reservations={reservations} tables={tables} handleUpdateTable={handleUpdateTable}/>
       </Route>
       <Route path="/reservations/new">
-        <NewReservation />
+        <NewReservation tables={tables} />
       </Route>
     </Switch>
   )
