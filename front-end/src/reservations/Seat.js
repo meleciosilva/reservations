b6c614c67bab6a1cdf728a2746b3d44f2d056d32
meleciosilva <mpsilva94@gmail.com>
2021-06-07 23:43:21 -0400

    add 'type="button"' to cancel button

--- front-end/src/reservations/Seat.js
+++ front-end/src/reservations/Seat.js
@@ -22,24 +22,25 @@ function Seat({ tables, handleUpdateTable }) {
       .then((updatedTables) => updatedTables[0])
       .then((handleUpdateTable))
       .then(() => history.push("/dashboard"))
-      .catch(err => setErrors(err))
+      .catch(err => setErrors([err]))
   }
   
+  if (!tables) return null;
   return (
-    <div className="row">
+    <div>
       <h1>Select A Table</h1>
       <hr />
       <ErrorAlert errors={errors} />
 
       <form onSubmit={handleSubmit}>
         <div className="col-md-12 mb-2">
-          <select name="table_id" className="form-select" aria-label="Default select example" value={tableId} onChange={handleChange}>
-            <option>Open to Select a Table Number</option>
-            { tables && tables.map(table => <option key={table.table_id} id={table.table_id}>{table.table_name} - {table.capacity}</option>) }
+          <select name="table_id" className="form-select" onChange={handleChange}>
+            <option defaultValue>Open to Select a Table Number</option>
+            { tables.map(table => <option key={table.table_id} id={table.table_id}>{table.table_name} - {table.capacity}</option>) }
           </select>
         </div>
         <div className="col-md-12">
-          <button className="btn btn-secondary mr-2" onClick={ () => history.goBack() }>Cancel</button>
+          <button type="button" className="btn btn-secondary mr-2" onClick={ () => history.goBack() }>Cancel</button>
           <button type="submit" className="btn btn-primary">Submit</button>
         </div>
       </form>
