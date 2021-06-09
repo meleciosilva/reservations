cf41fe1f29b51bd39d046c2519363af73e30e3c6
meleciosilva <mpsilva94@gmail.com>
2021-06-09 17:03:31 -0400

    update: handleFreeTableAndFinishReservation

--- front-end/src/dashboard/ListTables.js
+++ front-end/src/dashboard/ListTables.js
@@ -1,4 +1,4 @@
-function ListTables( {tables, handleFinish} ) {
+function ListTables({ tables, handleFreeTableAndFinishReservation }) {
   
   if (!tables) return null;
   const sortedTables = tables.sort((a, b) => a.table_name.localeCompare(b.table_name));
@@ -14,7 +14,7 @@ function ListTables( {tables, handleFinish} ) {
                 <h5 className="card-title">{ table.table_name }</h5>
                 <p className="card-text font-weight-bold" data-table-id-status={table.table_id} >{ !table.reservation_id ? "Free" : "Occupied" }</p>
                 { table.reservation_id && 
-                  <button type="button" className="btn btn-danger" data-table-id-finish={table.table_id} onClick={() => handleFinish(table.table_id)}>Finish</button> 
+                  <button type="button" className="btn btn-danger" data-table-id-finish={table.table_id} onClick={() => handleFreeTableAndFinishReservation(table.table_id)}>Finish</button> 
                 }
               </div>
             </div>
