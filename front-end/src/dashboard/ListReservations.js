a5e84a9bbb170cfd31104558f776f2ea71c191b0
meleciosilva <mpsilva94@gmail.com>
2021-06-08 22:03:31 -0400

    add consitional render for Seat button

--- front-end/src/dashboard/ListReservations.js
+++ front-end/src/dashboard/ListReservations.js
@@ -6,23 +6,32 @@ function ListReservations({ reservations }) {
   return (
     <div>
       <h2>Reservations</h2>
-      { reservations.map(reservation => (
-        <div className="card mb-2" key={ reservation.reservation_id }>
-          <div className="card-header d-flex justify-content-between">
-            <h5 className="font-weight-bold">{ reservation.first_name } { reservation.last_name }</h5>
-            <h5 className="font-weight-bold">Party: { reservation.people }</h5>
+      <div className="row">
+        { reservations.map(reservation => (
+          <div className="col-xs-12 col-md-6" key={ reservation.reservation_id }>
+            <div className="card mb-2">
+              <div className="card-header d-flex justify-content-between">
+                <h5 className="font-weight-bold">{ reservation.first_name } { reservation.last_name }</h5>
+                <h5 className="font-weight-bold">Party: { reservation.people }</h5>
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">Time</h5>
+                <p className="card-text">{ reservation.reservation_time }</p>
+                <h5 className="card-title">Contact Number</h5>
+                <p className="card-text">{ reservation.mobile_number }</p>
+                <h5 className="card-title">Status</h5>
+                <p className="card-text" data-reservation-id-status={reservation.reservation_id} >{ reservation.status.toUpperCase() }</p>
+              </div>
+              <div className="col-md-12 mb-2">
+                { reservation.status === "booked"
+                  ? <Link className="btn btn-success" to={`/reservations/${reservation.reservation_id}/seat`}>Seat</Link>
+                  : null
+                }
+              </div>
+            </div>
           </div>
-          <div className="card-body">
-            <h5 className="card-title">Time</h5>
-            <p className="card-text">{ reservation.reservation_time }</p>
-            <h5 className="card-title">Contact Number</h5>
-            <p className="card-text">{ reservation.mobile_number }</p>
-          </div>
-          <div className="col-md-12 mb-2">
-            <Link className="btn btn-success" to={`/reservations/${reservation.reservation_id}/seat`}>Seat</Link>
-          </div>
-        </div>
-      )) }
+        )) }
+      </div>
     </div>
   )
 }
