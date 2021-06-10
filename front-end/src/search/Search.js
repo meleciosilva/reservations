e09110fa9c0ee4b0105236fca84b29d2d5aac5c4
meleciosilva <mpsilva94@gmail.com>
2021-06-10 11:09:48 -0400

    add Search

---
+++ front-end/src/search/Search.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import { Switch, Route } from "react-router-dom";
+import { listAllReservations } from "../utils/api";
+import SearchBar from "./SearchBar";
+import ListReservations from "../shared/ListReservations";
+import ErrorAlert from "../shared/ErrorAlert";
+
+function Search() {
+
+  const [number, setNumber] = useState(null);
+  const [reservations, setReservations] = useState(null);
+  const [errors, setErrors] = useState(null);
+  
+  useEffect(fetchReservations, [number]);
+
+  function fetchReservations() {
+    const abortController = new AbortController();
+    setErrors(null);
+    listAllReservations(number, abortController.signal)
+      .then(setReservations)
+      .catch(setErrors);
+    return () => abortController.abort();
+  }
+
+  function handleFind(event, phoneNumber) {
+    event.preventDefault();
+    setNumber(phoneNumber);
+  }
+
+  return(
+    <Switch>
+      <Route exact={true} path="/search">
+        <SearchBar handleFind={handleFind}/>
+        <ErrorAlert errors={errors} />
+        <ListReservations reservations={reservations}/>
+      </Route>
+    </Switch>
+  )
+}
+
+export default Search;
