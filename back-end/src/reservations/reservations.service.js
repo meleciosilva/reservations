d6c3962f09ed362f9c39e27b4f84d80e17830421
meleciosilva <mpsilva94@gmail.com>
2021-06-10 09:32:15 -0400

    add search

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -33,10 +33,20 @@ function destroy(reservation_id) {
     .del();
 }
 
+function search(mobile_number) {
+  return knex("reservations")
+    .whereRaw(
+      "translate(mobile_number, '() -', '') like ?",
+      `%${mobile_number.replace(/\D/g, "")}%`
+    )
+    .orderBy("reservation_date");
+}
+
 module.exports = {
   list,
   read,
   create,
   update,
-  destroy
+  destroy,
+  search,
 }
