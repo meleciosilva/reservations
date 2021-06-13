ca47a46629d767f81a9b295b5e0b70764762d75c
meleciosilva <mpsilva94@gmail.com>
2021-06-13 15:57:27 -0400

    update spacing

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -8,8 +8,8 @@ function Dashboard(props) {
   return (
     <main>
       <h1>Dashboard</h1>
-      <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for { props.date }</h4>
+      <div className="my-4">
+        <h2>Reservations for { props.date }</h2>
       </div>
       <ButtonGroup date={props.date} handleDate={props.handleDate}/>
       <ErrorAlert error={props.errors} />
