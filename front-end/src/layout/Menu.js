2bf323b044703a1d949357bfd35701db710ce6c2
meleciosilva <mpsilva94@gmail.com>
2021-06-13 15:58:36 -0400

    update navbar

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -4,49 +4,39 @@ import { Link } from "react-router-dom";
 
 function Menu() {
   return (
-    <nav className="navbar navbar-dark align-items-start p-0">
-      <div className="container-fluid d-flex flex-column p-0">
-        <Link
-          className="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0"
-          to="/"
-        >
-          <div className="sidebar-brand-text mx-3">
-            <span>Periodic Tables</span>
-          </div>
-        </Link>
-        <hr className="sidebar-divider my-0" />
-        <ul className="nav navbar-nav text-light" id="accordionSidebar">
-          <li className="nav-item">
-            <Link className="nav-link" to="/dashboard">
-              <span className="oi oi-dashboard" />
-              &nbsp;Dashboard
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/search">
-              <span className="oi oi-magnifying-glass" />
-              &nbsp;Search
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/reservations/new">
-              <span className="oi oi-plus" />
-              &nbsp;New Reservation
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/tables/new">
-              <span className="oi oi-layers" />
-              &nbsp;New Table
-            </Link>
-          </li>
-        </ul>
-        <div className="text-center d-none d-md-inline">
-          <button
-            className="btn rounded-circle border-0"
-            id="sidebarToggle"
-            type="button"
-          />
+    <nav className="navbar navbar-dark navbar-expand-lg accentBackground">
+      <div className="container-fluid" id="navBar">
+        <Link className="navbar-brand" to="/dashboard"><h1>Periodic Tables</h1></Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item px-3">
+              <Link className="nav-link active" aria-current="page" to="/dashboard">
+                <span className="oi oi-dashboard pr-2" />
+                <span>Dashboard</span>
+              </Link>
+            </li>
+            <li className="nav-item px-3">
+              <Link className="nav-link" to="/search">
+                <span className="oi oi-magnifying-glass pr-2" />
+                <span>Search</span>
+              </Link>
+            </li>
+            <li className="nav-item px-3">
+              <Link className="nav-link" to="/reservations/new">
+                <span className="oi oi-plus pr-2" />
+                <span>New Reservation</span>
+              </Link>
+            </li>
+            <li className="nav-item px-3">
+              <Link className="nav-link" to="/tables/new">
+                <span className="oi oi-layers pr-2" />
+                <span>New Table</span>
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
